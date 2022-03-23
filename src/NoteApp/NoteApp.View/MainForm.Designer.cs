0b08fa62c0c02b84b297b56c41b84f5c671aca5c
Mugivarra <tin.in00@mail.ru>
2022-03-23 17:35:45 +0700

    Work start

---
+++ src/NoteApp/NoteApp.View/MainForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace NoteApp.View
+{
+	partial class MainForm
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+			this.FileScriptBox = new System.Windows.Forms.ToolStripComboBox();
+			this.toolStripComboBox2 = new System.Windows.Forms.ToolStripComboBox();
+			this.toolStripComboBox3 = new System.Windows.Forms.ToolStripComboBox();
+			this.menuStrip2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 15;
+			this.listBox1.Location = new System.Drawing.Point(12, 65);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(149, 334);
+			this.listBox1.TabIndex = 0;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(106, 38);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 23);
+			this.comboBox1.TabIndex = 1;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 41);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(88, 15);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Show category:";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(167, 83);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(599, 364);
+			this.richTextBox1.TabIndex = 4;
+			this.richTextBox1.Text = "";
+			// 
+			// menuStrip2
+			// 
+			this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileScriptBox,
+            this.toolStripComboBox2,
+            this.toolStripComboBox3});
+			this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip2.Name = "menuStrip2";
+			this.menuStrip2.Size = new System.Drawing.Size(800, 27);
+			this.menuStrip2.TabIndex = 6;
+			this.menuStrip2.Text = "menuStrip2";
+			// 
+			// FileScriptBox
+			// 
+			this.FileScriptBox.AutoToolTip = true;
+			this.FileScriptBox.Name = "FileScriptBox";
+			this.FileScriptBox.Size = new System.Drawing.Size(75, 23);
+			this.FileScriptBox.Text = "File";
+			// 
+			// toolStripComboBox2
+			// 
+			this.toolStripComboBox2.Name = "toolStripComboBox2";
+			this.toolStripComboBox2.Size = new System.Drawing.Size(121, 23);
+			// 
+			// toolStripComboBox3
+			// 
+			this.toolStripComboBox3.Name = "toolStripComboBox3";
+			this.toolStripComboBox3.Size = new System.Drawing.Size(121, 150);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.menuStrip2);
+			this.Name = "MainForm";
+			this.Text = "Form1";
+			this.menuStrip2.ResumeLayout(false);
+			this.menuStrip2.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.MenuStrip menuStrip2;
+		private System.Windows.Forms.ToolStripComboBox FileScriptBox;
+		private System.Windows.Forms.ToolStripComboBox toolStripComboBox2;
+		private System.Windows.Forms.ToolStripComboBox toolStripComboBox3;
+	}
+}
+
